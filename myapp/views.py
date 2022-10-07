623196aa93d2b38c4ebbb2111648384ef326a1be
Gizar <gizarir@gmail.com>
2022-10-07 20:03:49 +0300

    Myapp: setuped djangorestframework

---
+++ myapp/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+
+from .serializers import *
+from .models import *
+
+
+class SchoolViewset(viewsets.ModelViewSet):
+    queryset = School.objects.all()
+    serializer_class = SchoolSerializer
+
+class SClassViewset(viewsets.ModelViewSet):
+    queryset = SClass.objects.all()
+    serializer_class = SClassSerializer
+
+class StudentViewest(viewsets.ModelViewSet):
+   queryset = Student.objects.all()
+   serializer_class = SClassSerializer
